a3e9a2a8a8f83c71f7b337579276a74c8502c1c7
Kliver Daniel Giron Castro <kliver.giron1@correo.icesi.edu.co>
2019-02-09 19:17:44 -0500

    Modelo creado con las clases de los métodos de ordenamiento y se conecto el modelo con el proyecto ExperimentoMetOrdenamiento para usarlos directamente.

---
+++ Experimento/model/Model.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model
+{
+    public class Model
+    {
+
+        private HeapSort heap;
+        private MergeSort merge;
+
+        public Model()
+        {
+            heap = new HeapSort();
+            merge = new MergeSort();
+        }
+
+        public long MergeSort(int[] numbers, int left, int right)
+        {
+            long time = nanoTime();
+            int[] arrC = new int[numbers.Length];
+            numbers.CopyTo(arrC, 0);
+            int[] arr = merge.Sort(numbers,left,right);
+            if (Enumerable.SequenceEqual(arr, arrC))
+            {
+                return nanoTime() - time;
+            }
+            return -1L;
+        }
+
+        public long HeapSort(int[] numbers)
+        {
+            long time = nanoTime();
+            int[] arrC = new int[numbers.Length];
+            numbers.CopyTo(arrC,0);
+            int[] arr = heap.Sort(numbers);
+            Array.Sort(arrC);
+            if (Enumerable.SequenceEqual(arr,arrC))
+            {
+                return nanoTime()-time;
+            }
+            return -1L;
+        }
+
+        private static long nanoTime()
+        {
+            long nano = 10000L * Stopwatch.GetTimestamp();
+            nano /= TimeSpan.TicksPerMillisecond;
+            nano *= 100L;
+            return nano;
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
